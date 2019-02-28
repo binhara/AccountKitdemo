40ceb0b51d3a1e93a58500228408dab338cb2b80
alessandro <binhara@gmail.com>
2019-02-28 16:02:07 -0300

    primeira versao funcionado

--- AppAccoutKitDemo/AppAccoutKitDemo/MainActivity.cs
+++ AppAccoutKitDemo/AppAccoutKitDemo/MainActivity.cs
@@ -1,19 +1,155 @@
 ﻿using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
+using Android.OS;
+using Com.Facebook.Accountkit;
+using Android.Content;
+using Com.Facebook.Accountkit.UI;
+using Java.Lang;
+using Android.Support.V7.App;
+using Android.Gms.Auth.Api;
+
 
 namespace AppAccoutKitDemo
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public partial class MainActivity : AppCompatActivity
     {
+        
+        public static int APP_REQUEST_CODE = 99;
+
+        //TextView txtResult;
+        Button btnLoginWithSms;
+        Button btnLoginWithEmail;
+
+        public MainActivity()
+        {
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            btnLoginWithSms = FindViewById<Button>(Resource.Id.button1);
+            btnLoginWithEmail = FindViewById<Button>(Resource.Id.button2);
+            btnLoginWithSms.Click += delegate
+            {
+                //txtResult.Visibility = Android.Views.ViewStates.Invisible;
+                LoginWithPhoneNumber();
+            };
+            btnLoginWithEmail.Click += delegate
+            {
+                //txtResult.Visibility = Android.Views.ViewStates.Invisible;
+                LoginWithEmail();
+            };
+        }
+
+        public void LoginWithPhoneNumber()
+        {
+            var intent = new Intent(this, typeof(AccountKitActivity));
+
+            var configurationBuilder = new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                LoginType.Phone,
+                AccountKitActivity.ResponseType.Token);
+
+            intent.PutExtra(
+                AccountKitActivityBase.AccountKitActivityConfiguration,
+                configurationBuilder.Build());
+
+            StartActivityForResult(intent, APP_REQUEST_CODE);
+        }
+
+        public void LoginWithEmail()
+        {
+            var intent = new Intent(this, typeof(AccountKitActivity));
+            var configurationBuilder = new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                LoginType.Email,
+                AccountKitActivity.ResponseType.Token);
+            intent.PutExtra(
+                AccountKitActivityBase.AccountKitActivityConfiguration,
+                configurationBuilder.Build());
+
+            StartActivityForResult(intent, APP_REQUEST_CODE);
+        }
+
+        public void Logout()
+        {
+            AccountKit.LogOut();
+        }
+
+        public void GetCurrentAccount()
+        {
+            AccountKit.GetCurrentAccount(this);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode != APP_REQUEST_CODE)
+            {
+                return;
+            }
+
+            var jobj = data.GetParcelableExtra(AccountKitLoginResult.ResultKey);
+            IAccountKitLoginResult loginResult = Android.Runtime.Extensions.JavaCast<IAccountKitLoginResult>(jobj);
+            var toastMessage = "Hello, AccountKit.";
+
+            if (loginResult.Error != null)
+            {
+                toastMessage = loginResult.Error.ErrorType.Message;
+                //ShowErrorActivity(loginResult.Error);
+            }
+            else if (loginResult.WasCancelled())
+            {
+                toastMessage = "Login Cancelled";
+            }
+            else
+            {
+                if (loginResult.AccessToken != null)
+                {
+                    toastMessage = "Success:" + loginResult.AccessToken.AccountId;
+                }
+                else
+                {
+                    toastMessage = string.Format(
+                        "Success:{0}...",
+                        loginResult.AuthorizationCode.Substring(0, 10));
+                }
+
+                GetCurrentAccount();
+            }
+
+            // Surface the result to your user in an appropriate way.
+            Toast.MakeText(
+                    this,
+                    toastMessage,
+                    ToastLength.Long)
+                .Show();
         }
     }
+
+    partial class MainActivity : IAccountKitCallback
+    {
+        public void OnError(AccountKitError p0)
+        {
+            Toast.MakeText(
+                this,
+                p0.ToString(),
+                ToastLength.Short
+            ).Show();
+        }
+
+        public void OnSuccess(Object p0)
+        {
+            var account = Android.Runtime.Extensions.JavaCast<Account>(p0);
+
+            RunOnUiThread(delegate
+            {
+                //txtResult.Visibility = Android.Views.ViewStates.Visible;
+                //txtResult.Text = account.Email ?? account.PhoneNumber.GetPhoneNumber();
+            });
+        }
+    }
+
 }
